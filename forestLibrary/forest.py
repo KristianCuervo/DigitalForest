eeb62424b501f0cfd791c1f42abac49ef779a278
AuthorityKind <54275464+AuthorityKind@users.noreply.github.com>
2025-05-12 15:58:00 +0200

    kinda fixed the constant popping of small trees when using shadow/light
    also changed have functions in forest that had to go through the forest 2d array because I wanted to.

--- forestLibrary/forest.py
+++ forestLibrary/forest.py
@@ -50,15 +50,15 @@ class Forest:
         It should be investigated whether the trees should also start at
         age 1 or at random ages.
         """
-        for i in range(1, self.size+1):
-            for j in range(1, self.size+1):
-                # 1 --> self.size+1 are the non-boundary cells
-                
-                if np.random.rand() < self.initial_population:
-                    # Given a wanted population probability distribution, spawn random trees
-                    species_name = random.choice(self.active_species)
-                    genes = get_species_params(species_name, param_dict=reduced_SPECIES)
-                    self.grid[i, j] = SPECIES_CLASS[species_name](genes=genes)
+        def inner(self, i, j):
+            # 1 --> self.size+1 are the non-boundary cells    
+            if np.random.rand() < self.initial_population:
+                # Given a wanted population probability distribution, spawn random trees
+                species_name = random.choice(self.active_species)
+                genes = get_species_params(species_name, param_dict=reduced_SPECIES)
+                self.grid[i, j] = SPECIES_CLASS[species_name](genes=genes)
+
+        self.go_through_forest(inner)
 
     def update_sunlight(self):
         """
@@ -67,10 +67,13 @@ class Forest:
         and the angle of the sun. This is required to calculate the shadow.
         """
         sunlight_grid = np.zeros((self.size+2, self.size+2))
-        for i in range(1, self.size+1):
-            for j in range(1, self.size+1):
-                if self.grid[i, j] is not None:
+        def inner(self, i, j):
+            if self.grid[i, j] is not None:
                     sunlight_grid[i, j] = self.grid[i, j].sunlight
+
+        self.go_through_forest(inner)
+
+
         self.sunlight_grid =  sunlight_grid
 
     def update_shadows(self):
@@ -84,10 +87,12 @@ class Forest:
         shadow_kernel = np.array([[0.05, 0.1, 0.05],
                                   [0.1, 0, 0.1],
                                   [0.05, 0.1, 0.05]])
-        for i in range(1, self.size+1):
-            for j in range(1, self.size+1):
-                if self.grid[i, j] is not None:
-                    self.grid[i, j].shadow = np.sum(shadow_kernel*self.sunlight_grid[i-1:i+2, j-1:j+2])
+
+        def inner(self, i, j):
+            if self.grid[i, j] is not None:
+                self.grid[i, j].shadow = np.sum(shadow_kernel*self.sunlight_grid[i-1:i+2, j-1:j+2])
+        
+        self.go_through_forest(inner)
     
     
     def death_or_growth(self):
@@ -95,27 +100,30 @@ class Forest:
         Determines whether each tree in the forest survives or dies.
         The trees that die are removed from the forest.
         """
-        for i in range(1, self.size+1):
-            for j in range(1, self.size+1):
-                if self.grid[i, j] is not None:
-                    if self.grid[i, j].survival_roll() == False:
-                        self.death_pool[i, j] = self.grid[i, j] # adds the final tree state before its death to a pool for rendering
-                        self.grid[i, j] = None # Kills the tree instance
-                    else:
-                        self.grid[i, j].grow(forest_season=self.season)
+        def inner(self, i, j):
+            if self.grid[i, j] is not None:
+                if self.grid[i, j].survival_roll() == False:
+                    self.death_pool[i, j] = self.grid[i, j] # adds the final tree state before its death to a pool for rendering
+                    self.grid[i, j] = None # Kills the tree instance
+                else:
+                    self.grid[i, j].grow(forest_season=self.season)
+        
+        self.go_through_forest(inner)
 
     def update_gene_pools(self):
         """
         Returns seperate lists of the instances of each species in the forest.
         """
         gene_pools = {}
-        for i in range(1, self.size+1):
-            for j in range(1, self.size+1):
-                if self.grid[i, j] is not None:
-                    species = self.grid[i, j].genes['species'] # this is a string of the species name 
-                    if species not in gene_pools:
-                        gene_pools[species] = [] # creates a new list for the species
-                    gene_pools[species].append(self.grid[i, j]) # appends tree instance to list
+        def inner(self, i, j):
+            if self.grid[i, j] is not None:
+                species = self.grid[i, j].genes['species'] # this is a string of the species name 
+                if species not in gene_pools:
+                    gene_pools[species] = [] # creates a new list for the species
+                gene_pools[species].append(self.grid[i, j]) # appends tree instance to list   
+             
+        self.go_through_forest(inner)
+
         self.gene_pools = gene_pools
     
     def spawn_new_trees(self):
@@ -124,18 +132,25 @@ class Forest:
         The trees are spawned in empty cells with a probability of spawn_probability.
         """
         self.update_gene_pools()
-        for i in range(1, self.size+1):
-            for j in range(1, self.size+1):
-                if self.grid[i, j] is None and np.random.rand() < self.spawn_probability:
-                    species = random.choice(list(self.gene_pools.keys()))
-                    current_gene_pool = self.gene_pools[species]
-                    if len(current_gene_pool) < 2:
-                        # Not enough parents
-                        print(species, "is extinct or near.")
-                        continue
-                    # Create a child tree from gene pool
-                    child_genes = self.genetic_algorithm.generate_children(current_gene_pool, 1)[0]
-                    self.grid[i, j] = SPECIES_CLASS[species](genes=child_genes)
+        
+        #This solution is bad and stupid, I apologize
+        shadow_kernel = np.array([[0.05, 0.1, 0.05],
+                                  [0.1, 0, 0.1],
+                                  [0.05, 0.1, 0.05]])
+
+        def inner(self, i, j):
+            if self.grid[i, j] is None and np.random.rand() < self.spawn_probability and self.sunlight_grid[i,j] >= np.sum(shadow_kernel*self.sunlight_grid[i-1:i+2, j-1:j+2]):
+                species = random.choice(list(self.gene_pools.keys()))
+                current_gene_pool = self.gene_pools[species]
+                if len(current_gene_pool) < 2:
+                    # Not enough parents
+                    print(species, "is extinct or near.")
+                    return
+                # Create a child tree from gene pool
+                child_genes = self.genetic_algorithm.generate_children(current_gene_pool, 1)[0]
+                self.grid[i, j] = SPECIES_CLASS[species](genes=child_genes)
+        
+        self.go_through_forest(inner)
     
     def update_season(self):
         old_season = self.season
@@ -161,3 +176,8 @@ class Forest:
         self.spawn_new_trees()
         self.update_season()
         self.gen += 1
+    
+    def go_through_forest(self, func):
+        for i in range(1, self.size+1):
+            for j in range(1, self.size+1):
+                func(self, i, j)
